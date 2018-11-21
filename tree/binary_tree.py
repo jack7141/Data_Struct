22eb492e7a3476a13e04a46a3266d2ea9178dc9c
Gogh Hwang <ghl92479@gmail.com>
2018-11-22 01:19:08 +0900

    11

---
+++ tree/binary_tree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, item, left=None, right=None):
+        self.item=item
+        self.left=left
+        self.right=right
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None        
+    
